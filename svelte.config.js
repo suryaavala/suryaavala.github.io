d6a02eb8bfc97b849b341ffab3782f3c05eebbf2
Surya Avala <suryaavala.dev@gmail.com>
2022-07-31 20:39:07 +1000

    feat: static site rendering w/ static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+// using svelejs/adapter-static to prerender the app
+// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
